4e4814216f58f86874275ccc6a026ec184814681
luattala <lattala@mail.austral.edu.ar>
2025-04-07 12:26:39 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,10 +3,8 @@ def check_vowels():
 	nombre = input()
 	#print(f'\n{nombre}')
 	nombre = nombre.lower()
-	print(f'contiene a: {"a" in nombre}')
-	print(f'contiene e: {"e" in nombre}')
-	print(f'contiene i: {"i" in nombre}')
-	print(f'contiene o: {"o" in nombre}')
-	print(f'contiene u: {"u" in nombre}')
-
-
+	print(f'contiene a:{"a" in nombre}')
+	print(f'contiene e:{"e" in nombre}')
+	print(f'contiene i:{"i" in nombre}')
+	print(f'contiene o:{"o" in nombre}')
+	print(f'contiene u:{"u" in nombre}')
